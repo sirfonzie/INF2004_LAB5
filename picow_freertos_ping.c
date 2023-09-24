0ce03c503f3b52ff2f5bc0e6395695be48c0fb84
sirfonzie <33278390+sirfonzie@users.noreply.github.com>
2023-09-25 05:19:55 +0800

    Add files via upload
    This is the modified code that includes all the tasks.

---
+++ picow_freertos_ping.c
@@ -0,0 +1,187 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+#include "lwip/ip4_addr.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "ping.h"
+#include "message_buffer.h"
+
+#include <stdio.h>
+#include "hardware/gpio.h"
+
+#include "hardware/adc.h"
+
+#define mbaTASK_MESSAGE_BUFFER_SIZE       ( 60 )
+
+#ifndef PING_ADDR
+#define PING_ADDR "142.251.35.196"
+#endif
+#ifndef RUN_FREERTOS_ON_CORE
+#define RUN_FREERTOS_ON_CORE 0
+#endif
+
+#define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
+
+static MessageBufferHandle_t xControlMessageBuffer;
+
+float read_onboard_temperature() {
+    
+    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+    const float conversionFactor = 3.3f / (1 << 12);
+
+    float adc = (float)adc_read() * conversionFactor;
+    float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+
+    return tempC;
+}
+
+void main_task(__unused void *params) {
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        return;
+    }
+    cyw43_arch_enable_sta_mode();
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        exit(1);
+    } else {
+        printf("Connected.\n");
+    }
+
+    ip_addr_t ping_addr;
+    ipaddr_aton(PING_ADDR, &ping_addr);
+    ping_init(&ping_addr);
+
+    while(true) {
+        // not much to do as LED is in another task, and we're using RAW (callback) lwIP API
+        vTaskDelay(100);
+    }
+
+    cyw43_arch_deinit();
+}
+
+void led_task(__unused void *params) {
+    while(true) {
+        vTaskDelay(3000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(3000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    }
+}
+
+void temp_task(__unused void *params) {
+    float temperature = 0.0;
+    char cString[ 15 ];
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+    adc_select_input(4);
+    while(true) {
+        vTaskDelay(1000);
+        temperature = read_onboard_temperature();
+        printf("Onboard temperature = %.02f C\n", temperature);
+        //sprintf(cString, "%0.2f",temperature);
+        xMessageBufferSend( /* The message buffer to write to. */
+            xControlMessageBuffer,
+            /* The source of the data to send. */
+            (void *) &temperature,
+            /* The length of the data to send. */
+            sizeof( temperature ),
+            /* The block time, should the buffer be full. */
+            0 );
+    }
+}
+
+void avg_task(__unused void *params) {
+    float fReceivedData;
+    size_t xReceivedBytes;
+    static float data[4] = {0};
+    static int index = 0;
+    static int count = 0;
+    float sum = 0;
+
+    while(true) {
+        xReceivedBytes = xMessageBufferReceive( /* The message buffer to receive from. */
+            xControlMessageBuffer,
+            /* Location to store received data. */
+            (void *) &fReceivedData,
+            /* Maximum number of bytes to receive. */
+            sizeof( fReceivedData ),
+            /* Ticks to wait if buffer is empty. */
+            portMAX_DELAY );
+
+            // Subtract the oldest element from sum
+            sum -= data[index];
+            
+            // Assign the new element to the buffer
+            data[index] = fReceivedData;
+            
+            // Add the new element to sum
+            sum += data[index];
+            
+            // Update the index
+            index = (index + 1) % 4; // make it circular
+            
+            // Increment count till it reaches FILTER_SIZE
+            if (count < 4) count++;
+
+            printf("Average Temperature = %0.2f C\n", sum / count);
+    }
+}
+
+void vLaunch( void) {
+    TaskHandle_t task;
+    xTaskCreate(main_task, "TestMainThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
+    TaskHandle_t ledtask;
+    xTaskCreate(led_task, "TestLedThread", configMINIMAL_STACK_SIZE, NULL, 7, &ledtask);
+    TaskHandle_t temptask;
+    xTaskCreate(temp_task, "TestTempThread", configMINIMAL_STACK_SIZE, NULL, 8, &temptask);
+    TaskHandle_t avgtask;
+    xTaskCreate(avg_task, "TestAvgThread", configMINIMAL_STACK_SIZE, NULL, 5, &avgtask);
+
+    xControlMessageBuffer = xMessageBufferCreate(mbaTASK_MESSAGE_BUFFER_SIZE);
+
+#if NO_SYS && configUSE_CORE_AFFINITY && configNUM_CORES > 1
+    // we must bind the main task to one core (well at least while the init is called)
+    // (note we only do this in NO_SYS mode, because cyw43_arch_freertos
+    // takes care of it otherwise)
+    vTaskCoreAffinitySet(task, 1);
+#endif
+
+    /* Start the tasks and timer running. */
+    vTaskStartScheduler();
+}
+
+int main( void )
+{
+    stdio_init_all();
+
+    /* Configure the hardware ready to run the demo. */
+    const char *rtos_name;
+#if ( portSUPPORT_SMP == 1 )
+    rtos_name = "FreeRTOS SMP";
+#else
+    rtos_name = "FreeRTOS";
+#endif
+
+#if ( portSUPPORT_SMP == 1 ) && ( configNUM_CORES == 2 )
+    printf("Starting %s on both cores:\n", rtos_name);
+    vLaunch();
+#elif ( RUN_FREERTOS_ON_CORE == 1 )
+    printf("Starting %s on core 1:\n", rtos_name);
+    multicore_launch_core1(vLaunch);
+    while (true);
+#else
+    printf("Starting %s on core 0:\n", rtos_name);
+    vLaunch();
+#endif
+    return 0;
+}
